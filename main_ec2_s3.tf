218a62049e28eb7bfbc5daee0d7cf9a1963e8e61
dushyantkumar <kumardushyant545@gmail.com>
2023-04-20 19:52:29 +0530

    adding new  files for S3 bucket

--- main_ec2_s3.tf
+++ main_ec2_s3.tf
@@ -9,14 +9,22 @@ teffaform{
 }
 
 provider "aws" {
-    region = "aws-east-1"
+    region = "aws-south-1"
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  count = 4
+  count = 1
   ami_id = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform_test_server_${count.index}"
+    Name = "terraform_batch_instance"
   }
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+bucket = "terraform-terraform-with-dushyantkumar-12345"
+tags={
+    Name = "terraform-terraform-with-dushyantkumar-12345"
+    Environment = "Dev"
+}  
 }
